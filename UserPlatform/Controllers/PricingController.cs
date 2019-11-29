749e9744deea8867db56062745e251b61b447508
Olle Strand <ollestrand02@gmail.com>
2019-11-29 19:00:22 +0100

    New Site Format

---
+++ UserPlatform/Controllers/PricingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserPlatform.Controllers
+{
+    public class PricingController : Controller
+    {
+        // GET: Pricing
+        public ActionResult Index() => View();
+
+        public ActionResult Basic() => View();
+        public ActionResult Professional() => View();
+        public ActionResult Business() => View();
+    }
+}
